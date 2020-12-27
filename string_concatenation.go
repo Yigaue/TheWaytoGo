9d92aded8fa456ad5337f001d02f7f9f0b26461c
Lekia <lekiagospel@gmail.com>
2020-12-27 18:47:07 +0100

    further concatenation

---
+++ string_concatenation.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	firstName := "Gospel"
+	lastName := "Lekia"
+
+	title  := "Mr"
+
+	var greet = "Hel" + "lo " + title + " " + firstName + " "
+
+	greet += lastName
+
+	println(greet)
+}
