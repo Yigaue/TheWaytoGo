6fb72e21463948cb031b3f3dccd681cf627b1e24
Lekia <lekiagospel@gmail.com>
2020-12-27 19:16:34 +0100

    personal practice

--- presuffix.go
+++ presuffix.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main () {
-	var str string =  "This is an example of a string"
-	fmt.Printf("T/F ? Does the string \"%s\" have prefix %s ? ", str, "Th")
-	fmt.Printf("%t\n", strings.HasPrefix(str, "Th"))
+	var str string  = "Go is a nice programming language"
+
+	fmt.Printf("T/F ? Does the string \"%s\" has prefix %s ? ", str, "Go")
+	fmt.Printf("%t\n", strings.HasPrefix(str, "Go"))
 }
