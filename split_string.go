e62d2ebc38519c46d5391e376aa2bccd1ea358a2
Lekia <lekiagospel@gmail.com>
2020-12-28 22:02:54 +0100

    split join string

--- split_string.go
+++ split_string.go
@@ -9,5 +9,5 @@ func main () {
 	var str = "A string with spaces between words"
 	fmt.Printf("%s\n", strings.Fields(str)) // on whitespace
 	str  = "A, string, with, comma, and spaces between words"
-	fmt.Printf("%s\n", strings.Split(str, ","))
+	fmt.Printf("%s\n", strings.Split(str, ",")) // other separator other than spaces
 }
---
+++ strings_splitjoin.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main () {
+	str := "The quick brown fox jumps over the lazy dog"
+	s1 := strings.Fields(str)
+
+	fmt.Printf("Splitted in slice: %v\n", s1)
+
+	for _, val := range s1 {
+		fmt.Printf("%s - ", val)
+	}
+
+	str2 := "Go1|The ABC of Go|25"
+	s12 := Strings.Split(str2, "|")
+	fmt.Printf("Splitted in slice: %v\n", s12)
+	for _, val := range s12 {
+		fmt.Printf("%s - ", val)
+	}
+	
+	fmt.Println()
+
+	str3 := strings.Join(s12, ";")
+	fmt.Printf("s12 joined ;: %s\n", str3)
+}
