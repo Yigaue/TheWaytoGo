ce08b435eac20d82ed101ab033c556fd3012ff2d
Lekia <lekiagospel@gmail.com>
2020-12-31 10:53:35 +0100

    breaking out of a loop with a break statement

---
+++ for3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+
+	var i int = 5
+	for {
+		i = i -1 
+		fmt.Printf("The variable i is now: %d \n", i)
+
+		if i < 0 {
+			break // without the break here is the loop runs forever
+		}
+	}
+}
