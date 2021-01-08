da013065efdeb678c2626fea04987558bbc22b29
Lekia <lekiagospel@gmail.com>
2021-01-08 14:31:24 +0100

    variable function

--- varnumpar.go
+++ varnumpar.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	x := Min(1, 3, 2, 0)
 	fmt.Printf("The minimum is: %d\n", x)
-	arr := []int{7, 9, 3, 5, 1}
+	arr := []int{7, 9, 3, 5, 2}
 	x = Min(arr...)
 	fmt.Printf("The minimum in the array arr is: %d", x)
 }
