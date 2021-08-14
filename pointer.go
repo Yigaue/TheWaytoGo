e65593bddc5a48494ef16e162326d110263dff16
lekia <lekiagospel@gmail.com>
2021-08-14 13:49:48 +0100

    add comment

--- pointer.go
+++ pointer.go
@@ -6,7 +6,8 @@ func main () {
 	var i1 = 5
 	fmt.Printf("An integer: %d, its location in memory: %p\n", i1, &i1)
 
-	var intP *int
-	intP = &i1
+	var intP *int // intP is a variable(pointer) that stores memory location of value(variable) of type int. Here it is declared but not assigned. The "*" indicates that intP is a pointer variable and not and ordinary variable. This means intP will store a pointer of value of type int.
+
+	intP = &i1 // Here we assigned the memory location of variable i1 to intP. That makes intP a pointer.
 	fmt.Printf("The value of memory location %p is %d\n", intP, *intP)
 }
