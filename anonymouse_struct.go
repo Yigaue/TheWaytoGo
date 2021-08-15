aef0148799aad9cc2778717960ac9f307ee440d4
lekia <lekiagospel@gmail.com>
2021-08-15 01:54:28 +0100

    finish up anonymouse struct

--- anonymouse_struct.go
+++ anonymouse_struct.go
@@ -10,3 +10,20 @@ creating a new data type. This type of structs
 are called anonymouse struct.
 -----------------------------
 */
+
+func main() {
+	staff3 := struct {
+		firstName string
+		lastName string
+		age int
+		salary int
+	}{
+		"Mary", // you can leave the name field 
+		//but you must follow the order define in the struct above
+		"Joe",
+		29,
+		4000,
+	}
+
+	fmt.Println("staff 3:", staff3)
+}
